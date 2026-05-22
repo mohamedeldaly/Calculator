2483424172557ea51cc0e269c08547d1b4003169
mohamed eldaly <21154454+mohamedeldaly@users.noreply.github.com>
2026-05-22 17:59:04 +0300

    add logic for second number

--- script.js
+++ script.js
@@ -28,40 +28,73 @@ const buttons = document.querySelectorAll("button");
 const div = document.querySelector("div");
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
-    if (e.target.textContent === "." && num1 === "") {
-      num1 = "0.";
-      div.textContent = num1;
-    } else if (
-      (e.target.textContent === "0" && num1 !== "") ||
-      e.target.textContent === "1" ||
-      e.target.textContent === "2" ||
-      e.target.textContent === "3" ||
-      e.target.textContent === "4" ||
-      e.target.textContent === "5" ||
-      e.target.textContent === "6" ||
-      e.target.textContent === "7" ||
-      e.target.textContent === "8" ||
-      e.target.textContent === "9" ||
-      (e.target.textContent === "." && !num1.includes("."))
-    ) {
-      num1 += e.target.textContent;
-      div.textContent = num1;
-    } else if (e.target.textContent === "clear") {
-      num1 = "";
-      div.textContent = "0";
-    } else if (
-      e.target.textContent === "+" ||
-      e.target.textContent === "-" ||
-      e.target.textContent === "*" ||
-      e.target.textContent === "/"
-    ) {
-      operator = e.target.textContent;
-    } else if (e.target.textContent === "⌫" && num1.length > 1) {
-      num1 = num1.slice(0, -1);
-      div.textContent = num1;
-    } else if (e.target.textContent === "⌫" && num1.length === 1) {
-      num1 = "";
-      div.textContent = "0";
+    if (operator === "") {
+      if (e.target.textContent === "." && num1 === "") {
+        num1 = "0.";
+        div.textContent = num1;
+      } else if (
+        (e.target.textContent === "0" && num1 !== "") ||
+        e.target.textContent === "1" ||
+        e.target.textContent === "2" ||
+        e.target.textContent === "3" ||
+        e.target.textContent === "4" ||
+        e.target.textContent === "5" ||
+        e.target.textContent === "6" ||
+        e.target.textContent === "7" ||
+        e.target.textContent === "8" ||
+        e.target.textContent === "9" ||
+        (e.target.textContent === "." && !num1.includes("."))
+      ) {
+        num1 += e.target.textContent;
+        div.textContent = num1;
+      } else if (e.target.textContent === "clear") {
+        num1 = "";
+        div.textContent = "0";
+      } else if (
+        e.target.textContent === "+" ||
+        e.target.textContent === "-" ||
+        e.target.textContent === "*" ||
+        e.target.textContent === "/"
+      ) {
+        operator = e.target.textContent;
+      } else if (e.target.textContent === "⌫" && num1.length > 1) {
+        num1 = num1.slice(0, -1);
+        div.textContent = num1;
+      } else if (e.target.textContent === "⌫" && num1.length === 1) {
+        num1 = "";
+        div.textContent = "0";
+      }
+    } else if (operator !== "") {
+      if (e.target.textContent === "." && num2 === "") {
+        num2 = "0.";
+        div.textContent = num2;
+      } else if (
+        (e.target.textContent === "0" && num2 !== "") ||
+        e.target.textContent === "1" ||
+        e.target.textContent === "2" ||
+        e.target.textContent === "3" ||
+        e.target.textContent === "4" ||
+        e.target.textContent === "5" ||
+        e.target.textContent === "6" ||
+        e.target.textContent === "7" ||
+        e.target.textContent === "8" ||
+        e.target.textContent === "9" ||
+        (e.target.textContent === "." && !num2.includes("."))
+      ) {
+        num2 += e.target.textContent;
+        div.textContent = num2;
+      } else if (e.target.textContent === "clear") {
+        num2 = "";
+        div.textContent = "0";
+      } else if (e.target.textContent === "⌫" && num2.length > 1) {
+        num2 = num2.slice(0, -1);
+        div.textContent = num2;
+      } else if (e.target.textContent === "⌫" && num2.length === 1) {
+        num2 = "";
+        div.textContent = "0";
+      }
     }
+    console.log(num1);
+    console.log(num2);
   });
 });
