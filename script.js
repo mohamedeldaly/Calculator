7ddc8c4f58eb5ea2f91538c6c733f0bc2295caec
mohamed eldaly <21154454+mohamedeldaly@users.noreply.github.com>
2026-05-22 16:56:30 +0300

    add logic for first number

--- script.js
+++ script.js
@@ -27,5 +27,25 @@ let operator = "";
 const buttons = document.querySelectorAll("button");
 const div = document.querySelector("div");
 buttons.forEach((button) => {
-  button.addEventListener("click", (e) => {});
+  button.addEventListener("click", (e) => {
+    if (e.target.textContent === "." && num1 === "") {
+      num1 = "0.";
+      div.textContent = num1;
+    } else if (
+      (e.target.textContent === "0" && num1 !== "") ||
+      e.target.textContent === "1" ||
+      e.target.textContent === "2" ||
+      e.target.textContent === "3" ||
+      e.target.textContent === "4" ||
+      e.target.textContent === "5" ||
+      e.target.textContent === "6" ||
+      e.target.textContent === "7" ||
+      e.target.textContent === "8" ||
+      e.target.textContent === "9" ||
+      (e.target.textContent === "." && !num1.includes("."))
+    ) {
+      num1 += e.target.textContent;
+      div.textContent = num1;
+    }
+  });
 });
